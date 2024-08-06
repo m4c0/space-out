5697e50dded9a5f158ebccd8586c2fa7fbfa98f8
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-06 19:24:04 +0100

    cute little astronaut

--- poc.cpp
+++ poc.cpp
@@ -1,6 +1,19 @@
 #pragma leco app
+#pragma leco add_resource "atlas.png"
+
 import casein;
 import quack;
+
+static unsigned data(quack::instance *is) {
+  *is = quack::instance{
+      .position{0, 0},
+      .size{1, 1},
+      .uv0{0.0f / 16.0f, 1.0f / 16.0f},
+      .uv1{1.0f / 16.0f, 2.0f / 16.0f},
+      .multiplier{1, 1, 1, 1},
+  };
+  return 1;
+}
 
 struct init {
   init() {
@@ -19,6 +32,6 @@ struct init {
     });
 
     atlas("atlas.png");
-    // data(??);
+    data(::data);
   }
-};
+} i;
