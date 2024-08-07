a612f1dfc831c1707c5ffedac723509b25ea8083
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-07 11:18:37 +0100

    free axis movement

--- poc.cpp
+++ poc.cpp
@@ -5,27 +5,42 @@ import casein;
 import dotz;
 import quack;
 
+enum buttons { B_UP = 0, B_DOWN, B_LEFT, B_RIGHT, B_COUNT };
+
 static dotz::vec2 player_pos{};
+static bool button_state[B_COUNT]{};
 
 static void redraw();
 
-static void move(float dx, float dy) {
-  player_pos = player_pos + dotz::vec2{dx, dy} * 0.5f;
-  redraw();
+static float axis(buttons n, buttons p) {
+  auto ns = button_state[n];
+  auto ps = button_state[p];
+
+  if (ns && !ps) return -1;
+  if (ps && !ns) return +1;
+  return 0;
 }
 
 static unsigned data(quack::instance *is) {
   *is = quack::instance{
-      .position = player_pos,
-      .size{1, 1},
-      .uv0{0.0f / 16.0f, 1.0f / 16.0f},
-      .uv1{1.0f / 16.0f, 2.0f / 16.0f},
-      .multiplier{1, 1, 1, 1},
+    .position = player_pos,
+    .size = { 1, 1 },
+    .uv0 = { 0.0f / 16.0f, 1.0f / 16.0f },
+    .uv1 = { 1.0f / 16.0f, 2.0f / 16.0f },
+    .multiplier = { 1, 1, 1, 1 },
   };
   return 1;
 }
 
 static void redraw() { quack::donald::data(::data); }
+
+static void process_input() {
+  dotz::vec2 d{ axis(B_LEFT, B_RIGHT), axis(B_UP, B_DOWN) };
+  if (dotz::length(d) < 0.001) return;
+
+  player_pos = player_pos + d * 0.5f;
+  redraw();
+}
 
 struct init {
   init() {
@@ -39,16 +54,23 @@ struct init {
 
     clear_colour({});
     push_constants({
-        .grid_pos = {0, 0},
-        .grid_size = {16, 16},
+        .grid_pos = { 0, 0 },
+        .grid_size = { 16, 16 },
     });
 
     atlas("atlas.png");
     data(::data);
 
-    handle(KEY_DOWN, K_W, [] { move(0, -1); });
-    handle(KEY_DOWN, K_S, [] { move(0, +1); });
-    handle(KEY_DOWN, K_A, [] { move(-1, 0); });
-    handle(KEY_DOWN, K_D, [] { move(+1, 0); });
+    handle(KEY_DOWN, K_W, [] { button_state[B_UP] = true; });
+    handle(KEY_DOWN, K_S, [] { button_state[B_DOWN] = true; });
+    handle(KEY_DOWN, K_A, [] { button_state[B_LEFT] = true; });
+    handle(KEY_DOWN, K_D, [] { button_state[B_RIGHT] = true; });
+
+    handle(KEY_UP, K_W, [] { button_state[B_UP] = false; });
+    handle(KEY_UP, K_S, [] { button_state[B_DOWN] = false; });
+    handle(KEY_UP, K_A, [] { button_state[B_LEFT] = false; });
+    handle(KEY_UP, K_D, [] { button_state[B_RIGHT] = false; });
+
+    handle(TIMER, process_input);
   }
 } i;
