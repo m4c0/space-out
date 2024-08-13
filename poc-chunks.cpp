d3ed24b28a245db52dd0b02d024ae58ac545055f
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-13 17:59:24 +0100

    chunks: pulling some draw logic

--- poc-chunks.cpp
+++ poc-chunks.cpp
@@ -2,12 +2,51 @@
 
 import casein;
 import dotz;
+import hai;
 import quack;
+
+static hai::varray<dotz::vec2> dots { 1024 };
 
 static unsigned data(quack::instance *is) {
   const auto b = is;
 
+  *is++ = {
+    .position = dotz::floor(quack::donald::mouse_pos()),
+    .size = { 1 },
+    .colour = { 1, 0, 0, 1 },
+  };
+
+  for (auto &d : dots) {
+    *is++ = {
+      .position = d + dotz::vec2 { 0.1f },
+      .size = { 0.8f },
+      .colour = { 0, 1, 0, 1 },
+    };
+  }
+
   return is - b;
+}
+
+static void redraw() { quack::donald::data(::data); }
+
+static void ctor() {
+  auto cursor = dotz::floor(quack::donald::mouse_pos());
+  for (auto i = 0; i < dots.size(); i++) {
+    if (dotz::length(dots[i] - cursor) < 0.001f) {
+      return;
+    }
+  }
+
+  dots.push_back(cursor);
+}
+static void dtor() {
+  auto cursor = dotz::floor(quack::donald::mouse_pos());
+  for (auto i = 0; i < dots.size(); i++) {
+    if (dotz::length(dots[i] - cursor) < 0.001f) {
+      dots[i] = dots.pop_back();
+      return;
+    }
+  }
 }
 
 static struct init {
@@ -24,6 +63,10 @@ static struct init {
     });
 
     data(::data);
+
+    handle(MOUSE_MOVE, &redraw);
+    handle(MOUSE_DOWN, M_LEFT, &ctor);
+    handle(MOUSE_DOWN, M_RIGHT, &dtor);
   }
 } i;
 
